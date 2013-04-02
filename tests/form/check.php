48c5621b02cc476d328277561553674fddeea796
Josh Yudaken <j@yud.co.za>
2013-04-02 17:42:00 +0200

    Additional data/check test cases

--- tests/form/check.php
+++ tests/form/check.php
@@ -2,18 +2,65 @@
 
 class FormCheckTest extends \PHPUnit_Framework_TestCase {
 
+  public function testEmpty() {
+    $form = new \Forms\Form(array());
+
+    // Check no data provided
+    $this->assertSame($form->check([]), []);
+
+    // Irrelevant data should not create errors
+    $this->assertSame($form->check(['irrelevant' => 'data']), []);
+  }
+
   public function testRequired() {
     $form = new \Forms\Form(array(
       'firstname' => 'Firstname',
       'surname' => ['Surname', 'required' => True],
     ));
-    $post = array(
+
+    // Surname is required
+    $this->assertSame($form->check(array(
       'firstname' => 'John',
-      'surname' => '',
+    )), array(
+      'surname' => 'The surname field is required.'
+    ));
+
+    // Empty input is still required
+    $this->assertSame($form->check(array(
+      'firstname' => 'John', 'surname' => '',
+    )), array(
+      'surname' => 'The surname field is required.'
+    ));
+
+    // Firstname not required
+    $this->assertSame($form->check(array(
+      'firstname' => '', 'surname' => 'Smith',
+    )), array(
+    ));
+  }
+
+  public function testCustomCheck() {
+    $post = array(
+      'firstname' => '',
+      'surname' => 'Smith',
     );
 
+    $form = new \Forms\Form(array(
+      'firstname' => array('Firstname', 'required' => True),
+    ), array(
+      'check' => function($data) use ($post) {
+        // Check we got given the same post data
+        $this->assertSame($data, $post);
+
+        // Return a random error about non-existant surname field
+        return array('surname' => 'There is a problem with your surname.');
+      }
+    ));
+
+    // Assert that we see both errors
     $this->assertSame($form->check($post), array(
-      'surname' => 'The surname field is required.'
+      'firstname' => 'The firstname field is required.',
+      'surname' => 'There is a problem with your surname.',
     ));
   }
 
--- tests/form/data.php
+++ tests/form/data.php
@@ -11,8 +11,28 @@ class FormDataTest extends \PHPUnit_Framework_TestCase {
       'surname' => 'Smith',
     );
 
+    // Assert that surname was dropped
     $this->assertSame($form->data($post), array(
       'firstname' => 'John'
+    ));
+  }
+
+  public function testCustomFill() {
+    // Try create a fill function which swaps firstname and surname
+    $form = new \Forms\Form(array(
+      'firstname' => 'Firstname'
+    ), array(
+      'fill' => function(&$data) {
+        $data['surname'] = $data['firstname'];
+        unset($data['firstname']);
+      }
+    ));
+
+    // Ensure that posting firstname, gives the other surname
+    $this->assertSame($form->data(array(
+      'firstname' => 'John'
+    )), array(
+      'surname' => 'John'
     ));
   }
 
